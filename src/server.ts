538bbc82173e9e094556a4aaa24e965c0767f984
mauricioarizaga@hotmail.com <maarizaga@teco.com.ar>
2024-09-20 00:42:12 -0300

    firsa commit

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,25 @@
-import Hapi from '@hapi/hapi'
-import { defineRoutes } from './routes'
+import Hapi from "@hapi/hapi";
+import { routes } from "./routes/index";
 
 const getServer = () => {
-    const server = Hapi.server({
-        host: 'localhost',
-        port: 3000,
-    })
-
-    defineRoutes(server)
-
-    return server
-}
+	const server = Hapi.server({
+		host: "localhost",
+		port: 8080
+	});
+	server.route(routes);
+	return server;
+};
 
 export const initializeServer = async () => {
-    const server = getServer()
-    await server.initialize()
-    return server
-}
+	const server = getServer();
+	await server.initialize();
+	return server;
+};
 
 export const startServer = async () => {
-    const server = getServer()
-    await server.start()
-    console.log(`Server running on ${server.info.uri}`)
-    return server
+	const server = getServer();
+
+	await server.start();
+	console.log(`Server running on ${server.info.uri}`);
+	return server;
 };
